5a58e94768c446d64cc80575ea92d95221f3fef7
shekhar agrawal <csa.jpr08@gmail.com>
2023-08-07 11:23:49 +0530

    Doing first setup of project

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is runnnig");
+});
+
+app.use("/api/products", productRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is runnnig on port ${port}`);
+});
